56b80cfe746fac68966c90ff25d37a2d65b71f26
AlefMemTav <190115548@aluno.unb.br>
2023-03-02 13:42:43 -0300

    Testando o service com um json de courses

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 import { Course } from '../model/course';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,15 @@ import { Course } from '../model/course';
 
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      { _id: '1', name: 'Angular', category: 'Frontend' }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+      .pipe(
+        first(),
+        tap(courses => console.log(courses))
+      );
   }
 }
