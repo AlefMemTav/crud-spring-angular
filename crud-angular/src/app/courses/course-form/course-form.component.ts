a7595c08731c2114b9453278e599553167e538e0
AlefMemTav <190115548@aluno.unb.br>
2023-03-03 16:48:58 -0300

    Adicionando Location

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -14,7 +15,8 @@ export class CourseFormComponent {
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -24,13 +26,19 @@ export class CourseFormComponent {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe({
-      next: result => console.log(result),
+      next: result => this.onSuccess(),
       error: () => this.onError(),
     });
   }
 
   onCancel() {
-    console.log('onCancel is true [in course-form.component]');
+    this.location.back();
+    //console.log('onCancel is true [in course-form.component]');
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Course created', '', { duration: 3000 });
+    this.onCancel();
   }
 
 
