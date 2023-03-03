6a9b256e18fa7aa3592fbfb3683337ab28daf3ad
AlefMemTav <190115548@aluno.unb.br>
2023-03-03 17:10:50 -0300

    Atualizando course-form para typed forms

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../services/courses.service';
 
@@ -11,18 +11,16 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CourseFormComponent {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location
-  ) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
-  }
+  ) { }
 
   onSubmit() {
     this.service.save(this.form.value).subscribe({
@@ -36,7 +34,7 @@ export class CourseFormComponent {
     //console.log('onCancel is true [in course-form.component]');
   }
 
-  private onSuccess(){
+  private onSuccess() {
     this.snackBar.open('Course created', '', { duration: 3000 });
     this.onCancel();
   }
