f8433d82764716dc8f09da955d2f25336969fd35
AlefMemTav <190115548@aluno.unb.br>
2023-03-03 16:28:02 -0300

    Criação do método create em CourseRepository

--- crud-spring/src/main/java/com/loiane/controller/CourseController.java
+++ crud-spring/src/main/java/com/loiane/controller/CourseController.java
@@ -2,8 +2,13 @@ package com.loiane.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.loiane.model.Course;
@@ -19,8 +24,14 @@ public class CourseController {
 	private final CourseRepository courseRepository;
 
 	@GetMapping
-	public List<Course> list() {
+	public @ResponseBody List<Course> list() {
 		return courseRepository.findAll();
+	}
+
+	@PostMapping
+	// @ReponseStatus(code = HttpStatus.CREATED)
+	public ResponseEntity<Course> create(@RequestBody Course course) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
 	}
 
 }
