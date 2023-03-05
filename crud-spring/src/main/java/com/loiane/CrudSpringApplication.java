1e1f02c7b1c147c822bedea2a9303f6a903983eb
AlefMemTav <190115548@aluno.unb.br>
2023-03-05 11:55:46 -0300

    Criado findById

--- crud-spring/src/main/java/com/loiane/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/loiane/CrudSpringApplication.java
@@ -14,13 +14,12 @@ public class CrudSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
-	
+
 	@Bean
 	CommandLineRunner initDatabase(CourseRepository courseRepository) {
 		return args -> {
 			courseRepository.deleteAll();
-			
-			
+
 			Course c = new Course();
 			c.setName("Angular");
 			c.setCategory("front-end");
